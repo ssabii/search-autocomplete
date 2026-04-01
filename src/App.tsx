139a63da320f4c982a773634bc4220eb9f595c49
ssabii <fhy1930@gmail.com>
2026-04-01 20:49:15 +0900

    feat: 검색 폼 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import "./App.css";
 import useSearchProducts from "./feature/product/useSearchProducts";
 import { ChangeEvent } from "react";
 import useDebouncedValue from "./hooks/useDebouncedValue";
 import SuggestionList from "./feature/product/SuggestionList";
+import { searchProducts } from "./api/products";
 
 // TODO: API import
 // import { searchProducts, Product } from './api/products';
@@ -18,11 +19,19 @@ function App() {
     setQuery(value);
   };
 
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = await searchProducts(query);
+
+    console.log(data);
+  };
+
   return (
     <div className="app">
       <h1>상품 검색</h1>
 
-      <div className="search-container">
+      <form className="search-container" onSubmit={handleSubmit}>
         <input
           type="text"
           value={query}
@@ -30,8 +39,10 @@ function App() {
           placeholder="상품을 검색하세요"
           className="search-input"
         />
-        <button className="search-button">검색</button>
-      </div>
+        <button type="submit" className="search-button">
+          검색
+        </button>
+      </form>
 
       {/* TODO: 자동완성 목록 구현 */}
       {isLoading ? (
