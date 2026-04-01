75160741a87a6ac5967bf149096817bb8831645b
ssabii <fhy1930@gmail.com>
2026-04-01 20:32:53 +0900

    feat: useSearchProducts hooks 구현

---
+++ src/feature/product/useSearchProducts.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { Product, searchProducts } from "../../api/products";
+
+function useSearchProducts(query?: string) {
+  const [data, setData] = useState<Product[] | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+
+    const getProducts = async (q: string) => {
+      setIsLoading(true);
+
+      const res = await searchProducts(q);
+
+      setData(res);
+      setIsLoading(false);
+    };
+
+    getProducts(query);
+  }, [query]);
+
+  return {
+    data,
+    isLoading,
+  };
+}
+
+export default useSearchProducts;
